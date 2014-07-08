0c27fecabe1c29ec919ef2cf585fe3e8b028d6cc
Jonas Rosland <jonas.rosland@gmail.com>
2014-07-08 10:15:40 -0400

    first test1

--- local_config.py
+++ local_config.py
@@ -1,5 +1,6 @@
 akia = 'AKIAIIYZMG3EFUWEHH5Q'
 secret = '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'
+
 #app keys
 twitter1 = 'lTOXgsG47EYxJTRUYflm5c21f'
 twitter2 ='x8u3Go8QKOZdfe7fOdhwbNEtkeQNnDLus6SWmgwl7FHi3NYwO2'
